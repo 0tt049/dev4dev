09af04f436ced7cb14e2b750c7246176aee01144
Caio Alves <caioalvesfig@gmail.com>
2023-03-01 16:59:44 -0300

    project model created

---
+++ db/migrate/20230301195749_create_projects.rb
@@ -0,0 +1,12 @@
+class CreateProjects < ActiveRecord::Migration[7.0]
+  def change
+    create_table :projects do |t|
+      t.string :name
+      t.text :description
+      t.references :user, null: false, foreign_key: true
+      t.references :slot, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
