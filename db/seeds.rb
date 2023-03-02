0d9730b7c72956a8e8d0f25d19a11912f881f7cd
Guilherme Muoio <gmmuoio@hotmail.com>
2023-03-02 12:00:02 -0300

    seed com faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Creating users..."
+10.times do
+  User.create!(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: 'password'
+  )
+end
+
+puts "Creating slots..."
+20.times do
+  user = User.order("RANDOM()").first
+  Slot.create!(
+    user_id: user.id,
+    price: rand(200..1000),
+    programming_language: Faker::ProgrammingLanguage.name,
+    start_date: Faker::Date.between(from: '2023-01-01', to: '2023-06-30'),
+    end_date: Faker::Date.between(from: '2023-07-01', to: '2023-12-31'),
+    start_time: Faker::Time.between(from: '8:00 AM', to: '6:00 PM'),
+    end_time: Faker::Time.between(from: '8:00 AM', to: '8:00 PM')
+  )
+end
+
+puts "Finished!"
