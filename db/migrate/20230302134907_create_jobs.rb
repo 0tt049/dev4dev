de51a08fd9428578efca0f105d948aaa20e5338c
victor canedo <victorcanedo1@hotmail.com>
2023-03-02 10:57:39 -0300

    Changed projects table to jobs and added its controller

--- db/migrate/20230302134907_create_jobs.rb
+++ db/migrate/20230302134907_create_jobs.rb
@@ -1,6 +1,6 @@
-class CreateProjects < ActiveRecord::Migration[7.0]
+class CreateJobs < ActiveRecord::Migration[7.0]
   def change
-    create_table :projects do |t|
+    create_table :jobs do |t|
       t.string :name
       t.text :description
       t.references :user, null: false, foreign_key: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_01_200143) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_02_134907) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "jobs", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.bigint "user_id", null: false
+    t.bigint "slot_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["slot_id"], name: "index_jobs_on_slot_id"
+    t.index ["user_id"], name: "index_jobs_on_user_id"
+  end
 
   create_table "slots", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -40,5 +51,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_01_200143) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "jobs", "slots"
+  add_foreign_key "jobs", "users"
   add_foreign_key "slots", "users"
 end
