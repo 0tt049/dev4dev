0bf6c04ed971155f7a28b39a75a13b3be6005d1f
Guilherme Muoio <gmmuoio@hotmail.com>
2023-03-03 16:23:08 -0300

    date validation

--- app/models/slot.rb
+++ app/models/slot.rb
@@ -5,44 +5,55 @@ class Slot < ApplicationRecord
   ]
   validates :price, :programming_language, :start_date, :end_date, :start_time, :end_time, presence: true
   validates :programming_language, acceptance: { accept: PROGRAMMING_LANGUAGES }
-  validates :start_date_cannot_be_in_the_past, presence: true
+  validate :end_date_after_start_date
+
 
   has_many :gigs
   belongs_to :user
 
   private
 
-  def start_date_cannot_be_in_the_past
-    if start_date.present? && start_date < Date.today
+  def end_date_after_start_date
+    return if end_date.blank? || start_date.blank?
+
+    if end_date < start_date
+      errors.add(:end_date, "must be after the start date")
+    elsif start_date < Date.today
       errors.add(:start_date)
     end
   end
 
-  # If autocomplete is made, use this array
-  # PROGRAMMING_LANGUAGES = %w[1C Enterprise
-  #   2-Dimensional Array
-  #   4D
-  #   ABAP
-  #   ABAP CDS
-  #   ABNF
-  #   ActionScript
-  #   Ada
-  #   Adblock Filter List
-  #   Adobe Font Metrics
-  #   Agda
-  #   AGS Script
-  #   AIDL
-  #   AL
-  #   Alloy
-  #   Alpine Abuild
-  #   Altium Designer
-  #   AMPL
-  #   AngelScript
-  #   Ant Build System
-  #   Antlers
-  #   ANTLR
-  #   ApacheConf
-  #   Apex
+      # def start_date_cannot_be_in_the_past
+      #   if start_date.present? && start_date < Date.today
+      #     errors.add(:start_date)
+      #   end
+      # end
+
+      # If autocomplete is made, use this array
+      # PROGRAMMING_LANGUAGES = %w[1C Enterprise
+      #   2-Dimensional Array
+      #   4D
+      #   ABAP
+      #   ABAP CDS
+      #   ABNF
+      #   ActionScript
+      #   Ada
+      #   Adblock Filter List
+      #   Adobe Font Metrics
+      #   Agda
+      #   AGS Script
+      #   AIDL
+      #   AL
+      #   Alloy
+      #   Alpine Abuild
+      #   Altium Designer
+      #   AMPL
+      #   AngelScript
+      #   Ant Build System
+      #   Antlers
+      #   ANTLR
+      #   ApacheConf
+      #   Apex
   #   API Blueprint
   #   APL
   #   Apollo Guidance Computer
