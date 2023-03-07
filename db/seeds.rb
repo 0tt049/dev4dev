672384d3e81628358e0a39db6cb687408e9ae933
Otavio Coelho <otavio.gmcoelho@gmail.com>
2023-03-07 11:34:50 -0300

    Implemented new seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,323 @@
-require 'uri'
-require 'json'
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+seed_users = [{
+"login" => "mojombo",
+"avatar_url" => "https://avatars.githubusercontent.com/u/1?v=4",
+"html_url" => "https://github.com/mojombo",
+"name" => "Tom Preston-Werner",
+"location" => "San Francisco",
+"email" => "tom@mojombo.com"
+},
+{
+"login" => "pjhyett",
+"avatar_url" => "https://avatars.githubusercontent.com/u/3?v=4",
+"html_url" => "https://github.com/pjhyett",
+"name" => "PJ Hyett",
+"location" => "San Francisco",
+"email" => "pj@hyett.com"
+},
+{
+"login" => "wycats",
+"avatar_url" => "https://avatars.githubusercontent.com/u/4?v=4",
+"html_url" => "https://github.com/wycats",
+"name" => "Yehuda Katz",
+"location" => "Portland, OR",
+"email" => "wycats@gmail.com"
+},
+{
+"login" => "ivey",
+"avatar_url" => "https://avatars.githubusercontent.com/u/6?v=4",
+"html_url" => "https://github.com/ivey",
+"name" => "Michael D. Ivey",
+"location" => "Tuscumbia, AL",
+"email" => "ivey@gweezlebur.com"
+},
+{
+"login" => "evanphx",
+"avatar_url" => "https://avatars.githubusercontent.com/u/7?v=4",
+"html_url" => "https://github.com/evanphx",
+"name" => "Evan Phoenix",
+"location" => "Los Angeles, CA",
+"email" => "evan@phx.io"
+},
+{
+"login" => "vanpelt",
+"avatar_url" => "https://avatars.githubusercontent.com/u/17?v=4",
+"html_url" => "https://github.com/vanpelt",
+"name" => "Chris Van Pelt",
+"location" => "San Francisco",
+"email" => "vanpelt@wandb.com"
+},
+{
+"login" => "wayneeseguin",
+"avatar_url" => "https://avatars.githubusercontent.com/u/18?v=4",
+"html_url" => "https://github.com/wayneeseguin",
+"name" => "Wayne E. Seguin",
+"location" => "Buffalo, NY",
+"email" => "wayneeseguin@gmail.com"
+},
+{
+"login" => "macournoyer",
+"avatar_url" => "https://avatars.githubusercontent.com/u/22?v=4",
+"html_url" => "https://github.com/macournoyer",
+"name" => "Marc-André Cournoyer",
+"location" => "Montreal, QC, Canada",
+"email" => "macournoyer@gmail.com"
+},
+{
+"login" => "takeo",
+"avatar_url" => "https://avatars.githubusercontent.com/u/23?v=4",
+"html_url" => "https://github.com/takeo",
+"name" => "Toby Sterrett",
+"location" => "Portland, OR",
+"email" => "tobysterrett@gmail.com"
+},
+{
+"login" => "caged",
+"avatar_url" => "https://avatars.githubusercontent.com/u/25?v=4",
+"html_url" => "https://github.com/caged",
+"name" => "Justin Palmer",
+"location" => "Places",
+"email" => "encytemedia@gmail.com"
+},
+{
+"login" => "anotherjesse",
+"avatar_url" => "https://avatars.githubusercontent.com/u/27?v=4",
+"html_url" => "https://github.com/anotherjesse",
+"name" => "Jesse Andrews",
+"location" => "San Francisco, CA",
+"email" => "anotherjesse@gmail.com"
+},
+{
+"login" => "lukas",
+"avatar_url" => "https://avatars.githubusercontent.com/u/29?v=4",
+"html_url" => "https://github.com/lukas",
+"name" => "Lukas Biewald",
+"location" => "San Francisco",
+"email" => "lukas@wandb.com"
+},
+{
+"login" => "jamesgolick",
+"avatar_url" => "https://avatars.githubusercontent.com/u/37?v=4",
+"html_url" => "https://github.com/jamesgolick",
+"name" => "James Golick",
+"location" => "New York",
+"email" => "jamesgolick@gmail.com"
+},
+{
+"login" => "atmos",
+"avatar_url" => "https://avatars.githubusercontent.com/u/38?v=4",
+"html_url" => "https://github.com/atmos",
+"name" => "Corey Donohoe",
+"location" => "NYC ",
+"email" => "atmos@atmos.org"
+},
+{
+"login" => "mojodna",
+"avatar_url" => "https://avatars.githubusercontent.com/u/45?v=4",
+"html_url" => "https://github.com/mojodna",
+"name" => "Seth Fitzsimmons",
+"location" => "Bend, OR",
+"email" => "seth@mojodna.net"
+},
+{
+"login" => "bmizerany",
+"avatar_url" => "https://avatars.githubusercontent.com/u/46?v=4",
+"html_url" => "https://github.com/bmizerany",
+"name" => "Blake Mizerany",
+"location" => "San Francisco, CA",
+"email" => "blake.mizerany@gmail.com"
+},
+{
+"login" => "jnewland",
+"avatar_url" => "https://avatars.githubusercontent.com/u/47?v=4",
+"html_url" => "https://github.com/jnewland",
+"name" => "Jesse Newland",
+"location" => "Austin, TX",
+"email" => "jesse@jnewland.com"
+},
+{
+"login" => "joshknowles",
+"avatar_url" => "https://avatars.githubusercontent.com/u/48?v=4",
+"html_url" => "https://github.com/joshknowles",
+"name" => "Josh Knowles",
+"location" => "Austin, TX",
+"email" => "joshknowles@gmail.com"
+},
+{
+"login" => "hornbeck",
+"avatar_url" => "https://avatars.githubusercontent.com/u/49?v=4",
+"html_url" => "https://github.com/hornbeck",
+"name" => "John Hornbeck",
+"location" => "Lawton, OK",
+"email" => "hornbeck@gmail.com"
+},
+{
+"login" => "jwhitmire",
+"avatar_url" => "https://avatars.githubusercontent.com/u/50?v=4",
+"html_url" => "https://github.com/jwhitmire",
+"name" => "Jeff Whitmire",
+"location" => "Rochester, NY",
+"email" => "jeff@jwhitmire.com"
+},
+{
+"login" => "knzconnor",
+"avatar_url" => "https://avatars.githubusercontent.com/u/53?v=4",
+"html_url" => "https://github.com/knzconnor",
+"name" => "Kenzi Connor",
+"location" => "Berkeley, CA",
+"email" => "kenzi@cloudcity.io"
+},
+{
+"login" => "bs",
+"avatar_url" => "https://avatars.githubusercontent.com/u/68?v=4",
+"html_url" => "https://github.com/bs",
+"name" => "Britt Selvitelle",
+"location" => "San Fransico, CA",
+"email" => "yap@bri.tt"
+},
+{
+"login" => "schacon",
+"avatar_url" => "https://avatars.githubusercontent.com/u/70?v=4",
+"html_url" => "https://github.com/schacon",
+"name" => "Scott Chacon",
+"location" => "Berlin, Germany",
+"email" => "schacon@gmail.com"
+},
+{
+"login" => "bruce",
+"avatar_url" => "https://avatars.githubusercontent.com/u/72?v=4",
+"html_url" => "https://github.com/bruce",
+"name" => "Bruce Williams",
+"location" => "Portland, OR",
+"email" => "brwcodes@gmail.com"
+},
+{
+"login" => "sam",
+"avatar_url" => "https://avatars.githubusercontent.com/u/73?v=4",
+"html_url" => "https://github.com/sam",
+"name" => "Sam Smoot",
+"location" => "Dallas, TX",
+"email" => "ssmoot@gmail.com"
+},
+{
+"login" => "mmower",
+"avatar_url" => "https://avatars.githubusercontent.com/u/74?v=4",
+"html_url" => "https://github.com/mmower",
+"name" => "Matt Mower",
+"location" => "Berkshire, England",
+"email" => "self@mattmower.com"
+},
+{
+"login" => "rabble",
+"avatar_url" => "https://avatars.githubusercontent.com/u/76?v=4",
+"html_url" => "https://github.com/rabble",
+"name" => "rabble",
+"location" => "Pōneke, Aotearoa",
+"email" => "evan@protest.net"
+},
+{
+"login" => "benburkert",
+"avatar_url" => "https://avatars.githubusercontent.com/u/77?v=4",
+"html_url" => "https://github.com/benburkert",
+"name" => "Ben Burkert",
+"location" => "NYC",
+"email" => "ben@benburkert.com"
+},
+{
+"login" => "indirect",
+"avatar_url" => "https://avatars.githubusercontent.com/u/78?v=4",
+"html_url" => "https://github.com/indirect",
+"name" => "André Arko",
+"location" => "San Francisco",
+"email" => "andre@arko.net"
+},
+{
+"login" => "fearoffish",
+"avatar_url" => "https://avatars.githubusercontent.com/u/79?v=4",
+"html_url" => "https://github.com/fearoffish",
+"name" => "Jamie van Dyke",
+"location" => "Skipton, UK",
+"email" => "me@fearof.fish"
+},
+{
+"login" => "ry",
+"avatar_url" => "https://avatars.githubusercontent.com/u/80?v=4",
+"html_url" => "https://github.com/ry",
+"name" => "Ryan Dahl",
+"location" => "New York City",
+"email" => "ry@tinyclouds.org"
+},
+{
+"login" => "peimei",
+"avatar_url" => "https://avatars.githubusercontent.com/u/84?v=4",
+"html_url" => "https://github.com/peimei",
+"name" => "E. James O'Kelly",
+"location" => "Palm Springs, CA",
+"email" => "james@railsjitsu.com"
+},
+{
+"login" => "brixen",
+"avatar_url" => "https://avatars.githubusercontent.com/u/85?v=4",
+"html_url" => "https://github.com/brixen",
+"name" => "Brian Shirai",
+"location" => "Portland, OR",
+"email" => "brian@rubinius.com"
+},
+{
+"login" => "outerim",
+"avatar_url" => "https://avatars.githubusercontent.com/u/88?v=4",
+"html_url" => "https://github.com/outerim",
+"name" => "Lee Jensen",
+"location" => "SL,UT",
+"email" => "lee@outerim.com"
+},
+{
+"login" => "sr",
+"avatar_url" => "https://avatars.githubusercontent.com/u/90?v=4",
+"html_url" => "https://github.com/sr",
+"name" => "Sir Sir",
+"location" => "Europe",
+"email" => "me@simonrozet.com"
+},
+{
+"login" => "lifo",
+"avatar_url" => "https://avatars.githubusercontent.com/u/91?v=4",
+"html_url" => "https://github.com/lifo",
+"name" => "Pratik",
+"location" => "Allen, TX",
+"email" => "pratiknaik@gmail.com"
+},
+{
+"login" => "rsl",
+"avatar_url" => "https://avatars.githubusercontent.com/u/92?v=4",
+"html_url" => "https://github.com/rsl",
+"name" => "Russell Norris",
+"location" => "Atlanta, GA",
+"email" => "sconds@gmail.com"
+},
+{
+"login" => "imownbey",
+"avatar_url" => "https://avatars.githubusercontent.com/u/93?v=4",
+"html_url" => "https://github.com/imownbey",
+"name" => "Ian Ownbey",
+"location" => "San Francisco, CA",
+"email" => "ian.ownbey@coinbase.com"
+},
+{
+"login" => "jm",
+"avatar_url" => "https://avatars.githubusercontent.com/u/95?v=4",
+"html_url" => "https://github.com/jm",
+"name" => "Jeremy McAnally",
+"location" => "Orlando, FL",
+"email" => "jeremymcanally@gmail.com"
+},
+{
+"login" => "kmarsh",
+"avatar_url" => "https://avatars.githubusercontent.com/u/100?v=4",
+"html_url" => "https://github.com/kmarsh",
+"name" => "Kevin Marsh",
+"location" => "Toledo, OH",
+"email" => "kevin.marsh@gmail.com"
+}]
 
 puts "Cleaning up database..."
 Slot.destroy_all
@@ -15,11 +326,22 @@ Gig.destroy_all
 puts "Database cleaned"
 
 puts "Creating users..."
-15.times do
+# 15.times do
+#   User.create!(
+#     name: seed_users[]
+#     email: Faker::Internet.email,
+#     password: 'password'
+#   )
+# end
+seed_users.each do |user|
   User.create!(
-    name: Faker::Name.name,
-    email: Faker::Internet.email,
-    password: 'password'
+    name: user["name"],
+    email: user["email"],
+    password: 'password',
+    login: user["login"],
+    avatar_url: user["avatar_url"],
+    html_url: user["html_url"],
+    location: user["location"]
   )
 end
 
